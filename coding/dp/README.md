a7db622e46195a9a276ce2485452136a266cda33
Paolo Maresca <plo.maresca@gmail.com>
2021-01-04 15:12:23 +0100

    Added Climbing Stairs with DP.

--- coding/dp/README.md
+++ coding/dp/README.md
@@ -4,5 +4,10 @@ A programming technique often used to solve optimization problems. To get famili
 See the theory section for more insights about DP.
 
 ## Easy
-
+- [Climbing Stairs](https://leetcode.com/problems/climbing-stairs/)
 ## Medium
+- [Unique Paths](https://leetcode.com/problems/unique-paths/)
+- [Longest Increasing Subsequence](https://leetcode.com/problems/longest-increasing-subsequence/)
+- [Longest Common Subsequence](https://leetcode.com/problems/longest-common-subsequence/)
+- [Word Break](https://leetcode.com/problems/word-break/)
+- [Coing Change](https://leetcode.com/problems/coin-change/) 
---
+++ coding/dp/climbing_stairs.py
@@ -0,0 +1,58 @@
+
+# On Leetcode: https://leetcode.com/problems/climbing-stairs/
+
+class Solution(object):
+    def __init__(self):
+        self.memo = {}
+    
+    def climb_stairs(self, n):
+        """
+        Adopting recursion, the recursion tree looks like:
+
+            3
+          2   1
+         1 0 0 -1
+        0 -1
+
+        Base cases are then n == 0 and n < 0. In the former
+        case, a solution is reached and unrolling the stack
+        is going to count back. In the latter case, an invalid
+        solution is reached, so unrolling the stack that branch
+        should not count. 
+        """
+        if n < 0:       # not acceptable as solution
+            return 0
+        if n == 0:      # acceptable as solution
+            return 1
+        
+        if n - 1 not in self.memo:
+            self.memo[n - 1] = self.climb_stairs(n - 1)
+        if n - 2 not in self.memo:
+            self.memo[n - 2] = self.climb_stairs(n - 2)
+        
+        return self.memo[n - 1] + self.memo[n - 2]
+
+def main():
+    s = Solution()
+
+    n = 2
+    print(s.climb_stairs(n))
+    n = 3
+    print(s.climb_stairs(n))
+    n = 4
+    """
+    For 4, the recursion tree looks like:
+
+                   4
+            3           2
+        2       1     1   0
+      1   0    0 -1  0 -1
+     1 -1
+    0 -1 
+    """
+    print(s.climb_stairs(n))
+    n = 6
+    print(s.climb_stairs(n))
+
+if __name__ == '__main__':
+    main()
