333f3f734e57389953bbfdba36604f3831b85793
Paolo Maresca <plo.maresca@gmail.com>
2021-01-06 09:53:35 +0100

    Added Number of Islands problem.

---
+++ coding/graph/nr_of_islands.py
@@ -0,0 +1,24 @@
+
+# On Leetcode: https://leetcode.com/problems/number-of-islands/
+
+class Solution(object):
+    def number_of_islands(self, grid):
+        pass
+
+def main():
+    grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+
+    grid = [
+        ["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]
+    ]
+
+if __name__ == '__main__':
+    main()
