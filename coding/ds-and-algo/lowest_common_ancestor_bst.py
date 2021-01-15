0ef847ff522d27d897a57b2451e7b2bd09d62228
Paolo Maresca <plo.maresca@gmail.com>
2021-01-15 19:08:40 +0100

    Added Remove Nth Node from List solution

--- coding/ds-and-algo/lowest_common_ancestor_bst.py
+++ coding/ds-and-algo/lowest_common_ancestor_bst.py
@@ -1,11 +1,76 @@
 
 # On Leetcode: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
+class Node(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution(object):
-    pass
+    def pretty_print(self, root, idx=0):
+        if not root:
+            return
+        
+        print('%s%s' % (''.join(['  ' for _ in range(idx)]), root.val))
+        self.pretty_print(root.left, idx + 1)
+        self.pretty_print(root.right, idx + 1)
+    
+    def build_tree(self, array, idx=0):
+        if idx >= len(array):
+            return None
+        
+        root = Node(array[idx])
+        root.left = self.build_tree(array, idx * 2 + 1)
+        root.right = self.build_tree(array, idx * 2 + 2)
+
+        return root
+
+    def lca(self, root, p, q):
+        
+        left = self.left(root.left, p, q)
+        right = self.right(root.right, p, q)
+
+        if left != -1:
+            return left
+        if right != -1:
+            return right
+        if p == root.val or p == root.left.val or p == root.right.val:
+            if q == root.left.val or q == root.right.val:
+                return root.val
+
+        return -1 
 
 def main():
-    pass
+    s = Solution()
+
+    array = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5] 
+    p = 2
+    q = 8
+    root = s.build_tree(array)
+    s.pretty_print(root)
+    print(s.lca(root, p, q))
+
+    array = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5] 
+    p = 2
+    q = 8
+    root = s.build_tree(array)
+    s.pretty_print(root)
+    print(s.lca(root, p, q))
+
+    array = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]
+    p = 2
+    q = 4
+    root = s.build_tree(array)
+    s.pretty_print(root)
+    print(s.lca(root, p, q))
+
+    array = [2, 1]
+    p = 2
+    q = 1
+    root = s.build_tree(array)
+    s.pretty_print(root)
+    print(s.lca(root, p, q))
 
 if __name__ == '__main__':
     main()
--- coding/ds-and-algo/remove_nth_node_from_end_list.py
+++ coding/ds-and-algo/remove_nth_node_from_end_list.py
@@ -1,11 +1,88 @@
 
 # On Leetcode: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
+class LinkedList(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
-    pass
+    def build_linkedlist(self, array, idx=0):
+        if idx >= len(array):
+            return None
+        
+        head = LinkedList(array[idx])
+        head.next = self.build_linkedlist(array, idx + 1)
+
+        return head
+
+    def pretty_print(self, head):
+        if not head:
+            return None
+        
+        return '%s->%s' % (head.val, self.pretty_print(head.next))
+
+    def length(self, head, idx=0):
+        if not head:
+            return idx
+        
+        return self.length(head.next, idx + 1)
+
+    def remove(self, head, n, m, idx=0, prev=None):
+        if not head:
+            return
+        if m == idx:            # got on the element to remove
+            prev.next = head.next
+            head.next = None
+        
+        self.remove(head.next, n, m, idx + 1, prev=head)
+
+    def remove_nth_element(self, head, n):
+        """
+        There are two ways of approaching the problem. Either a list is reversed
+        and the nth - 1 elementh is removed, then reversed again. Or, a list length
+        is calculated and the forward traversal index is derived to remove the node.
+        This implementation goes with the latter option.
+        """
+        l = self.length(head)
+        if l == 1 and n == 1:   # case of a single node list
+            return None
+
+        m = l - n               # get the node index
+        self.remove(head, n, m)
+
+        return head
 
 def main():
-    pass
+    s = Solution()
+
+    array = [1,2,3,4,5]
+    n = 2
+    head = s.build_linkedlist(array)
+    print(s.pretty_print(head))
+    head = s.remove_nth_element(head, n)
+    print(s.pretty_print(head))
+    
+    array = [1]
+    n = 1
+    head = s.build_linkedlist(array)
+    print(s.pretty_print(head))
+    head = s.remove_nth_element(head, n)
+    print(s.pretty_print(head))
+
+    array = [1,2]
+    n = 1
+    head = s.build_linkedlist(array)
+    print(s.pretty_print(head))
+    head = s.remove_nth_element(head, n)
+    print(s.pretty_print(head))
+
+    array = [1,2,3,4,5]
+    n = 4
+    head = s.build_linkedlist(array)
+    print(s.pretty_print(head))
+    head = s.remove_nth_element(head, n)
+    print(s.pretty_print(head))
 
 if __name__ == '__main__':
     main()
