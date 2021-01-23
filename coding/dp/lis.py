b9ccf4cb46b92aa9b53b021324b56ff7cc8f54cd
Paolo Maresca <plo.maresca@gmail.com>
2021-01-23 19:07:21 +0100

    Added Longest Increasing Subsequence O(N^2) solution

--- coding/dp/lis.py
+++ coding/dp/lis.py
@@ -19,17 +19,33 @@ class Solution(object):
         This algorithm can be implemented with recursion as well. It should run
         for all elements as pivot, and the max should be chosen.
         """
-        pass
+        max_cnt = -1
+        for idx1, num1 in enumerate(nums):
+          idx2 = idx1 + 1
+          cntr = 1
+          while idx2 < len(nums):
+            num2 = nums[idx2]
+            if num1 < num2:
+              cntr += 1
+              prev_num2 = nums[idx2 - 1]
+              if num2 < prev_num2:
+                cntr -= 1
+            idx2 += 1
+          max_cnt = max(max_cnt, cntr)
+        
+        return max_cnt
 
 def main():
     s = Solution()
 
     nums = [10,9,2,5,3,7,101,18]
-    print(s.lis(nums))
+    print(s.lis(nums))    # 4
+
     nums = [0,1,0,3,2,3]
-    print(s.lis(nums))
+    print(s.lis(nums))    # 4
+
     nums = [7,7,7,7,7,7,7]
-    print(s.lis(nums))
+    print(s.lis(nums))    # 1
 
 if __name__ == '__main__':
     main()
