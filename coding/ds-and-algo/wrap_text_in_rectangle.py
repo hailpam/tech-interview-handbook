413a1d6b5a6e0be19fee898d73c34c81ebc54e4f
Paolo Maresca <plo.maresca@gmail.com>
2021-01-25 16:13:11 +0100

    Added Wrap Text in Rectangle problem

---
+++ coding/ds-and-algo/wrap_text_in_rectangle.py
@@ -0,0 +1,17 @@
+"""
+String: Thisissometext
+
++-------+  \
+|Thisiss|   \ h pixels high
+|ometext|   /
++-------+  /
+
+\_______/
+  w pixels wide
+"""
+
+def getHeight(fs):
+  return # line height (int) of font at font size fs (int) 
+
+def getWidth(fs, c):
+  return # width (int) of character c at font size fs (int) 
