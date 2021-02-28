cc87f5e2a9b263d46ddff805fbb2255959d7f6cb
Paolo Maresca <plo.maresca@gmail.com>
2021-02-28 16:49:12 +0100

    Added Largest Triangle Area solution.

--- coding/array/largest_triangle_area.py
+++ coding/array/largest_triangle_area.py
@@ -2,10 +2,19 @@
 
 class Solution(object):
     def largest_triangle_area(self, points):
-        pass
+        x_max = -1
+        y_max = -1
+        for point in points:
+            x_max = max(x_max, point[0])
+            y_max = max(y_max, point[1])
+        
+        return x_max * y_max / 2
 
 def main():
-    pass
+    s = Solution()
+
+    points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+    print(s.largest_triangle_area(points))
 
 if __name__ == '__main__':
     main()
