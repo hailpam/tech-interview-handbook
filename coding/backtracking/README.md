e6859bb6085a4935e446e966ddbb5d48e047cbcc
Paolo Maresca <plo.maresca@gmail.com>
2021-02-28 12:41:46 +0100

    Added Letter Case Permutation solution.

--- coding/backtracking/README.md
+++ coding/backtracking/README.md
@@ -9,6 +9,7 @@ Backtracking helps solving problems that deal with constraints meanwhile increme
 - [Combinations](https://leetcode.com/problems/combinations/)
 - [Subsets](https://leetcode.com/problems/subsets/)
 - [Count Sorted Vowel String](https://leetcode.com/problems/count-sorted-vowel-strings/)
+- [Letter Case Permutation](https://stackoverflow.com/questions/522563/accessing-the-index-in-for-loops)
 ## Hard
 - [Maze Solver](https://leetcode.com/problems/escape-a-large-maze/)
 - [Sudoky Solver](https://leetcode.com/problems/sudoku-solver/)
---
+++ coding/backtracking/letter_case_permutation.py
@@ -0,0 +1,81 @@
+
+# On Leetcode: https://stackoverflow.com/questions/522563/accessing-the-index-in-for-loops
+
+class Solution(object):
+    def permute(self, string, permutations, idx=0, permutation=[]):
+        for i in range(idx, len(string)):           # upon recursion, it has to only look forward
+            c = string[i]                           # let's make a copy of the character
+            if not string[i].isnumeric():
+                permutation.append(c.lower())       # got to go on lowercase branch
+                self.permute(string, permutations, i + 1, permutation)
+                permutation.pop()
+
+                permutation.append(c.upper())       # got to go on uppercase branch
+                self.permute(string, permutations, i + 1, permutation)
+                permutation.pop()
+
+                break                               # got to break here, not needed to go futher (epxlored by recursion)
+            else:
+                permutation.append(c)               # it's number
+
+        if len(permutation) == len(string):         # got the full length permutated string
+            permutations.append(''.join(permutation))
+        if permutation:
+            permutation.pop()                       # remove actual char unrolling the recursive stack
+        
+    def letter_case_permutation(self, string):
+        """
+        The main idea of this algorithm is to build incrementally and recursively,
+        adopting backtracking, the permutations of the lower and uppercase letters
+        as they present themselves in the string.
+
+        Example: a1b2
+            a1
+                b2
+                B2
+
+            A1
+                b2
+                B2
+        
+        Example: a1b2c3
+            a1
+                b2
+                    c3
+                    C3
+                B2
+                    c3
+                    C3
+            A1
+                b2
+                    c3
+                    C3
+                B2
+                    c2
+                    C3
+        """
+        permutations = []
+        self.permute(string, permutations, 0, [])
+
+        return permutations
+
+def main():
+    s = Solution()
+
+    string = 'a1b2'
+    print(s.letter_case_permutation(string))
+
+    string = '3z4'
+    print(s.letter_case_permutation(string))
+
+    string = 'a1b2c3'
+    print(s.letter_case_permutation(string))
+
+    string = '12345'
+    print(s.letter_case_permutation(string))
+
+    string = '0'
+    print(s.letter_case_permutation(string))
+    
+if __name__ == '__main__':
+    main()
